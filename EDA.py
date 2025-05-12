e3d1c9c06d967220fd289711860417ca39f05f37
Gustavo Daniel <105076760+FeijasVrau@users.noreply.github.com>
2025-05-11 22:25:53 -0300

    Test

---
+++ EDA.py
@@ -0,0 +1,3 @@
+import pandas as pd  # Import the pandas library for data manipulation and analysis
+df = pd.read_csv ('train.csv') # Load the dataset into a pandas dataframe
+df.head(5) # Display the first 5 rows of the dataframe
